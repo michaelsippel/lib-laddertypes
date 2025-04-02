7b25b4472d6294d6817e99f3989469ed020fe900
Michael Sippel <micha@fragmental.art>
2025-04-02 13:58:58 +0200

    MorphismType: add strip_common_rungs()

--- src/morphism_sugared.rs
+++ src/morphism_sugared.rs
@@ -1,11 +1,6 @@
 use {
     crate::{
-        substitution_sugared::SugaredSubstitution,
-        sugar::{SugaredStructMember, SugaredTypeTerm},
-        unification::UnificationProblem,
-        unification_sugared::SugaredUnificationProblem,
-        unparser::*, TypeDict,
-        TypeID, TypeTerm
+        substitution_sugared::SugaredSubstitution, sugar::{SugaredStructMember, SugaredTypeTerm}, unification::UnificationProblem, unification_sugared::SugaredUnificationProblem, unparser::*, SugaredEnumVariant, TypeDict, TypeID, TypeTerm
     },
     std::{collections::HashMap, u64}
 };
@@ -19,6 +14,7 @@ pub struct SugaredMorphismType {
 }
 
 impl SugaredMorphismType {
+
     pub fn apply_subst(&self, σ: &impl SugaredSubstitution) -> SugaredMorphismType {
         SugaredMorphismType {
             src_type: self.src_type.clone().apply_subst(σ).clone(),
@@ -30,6 +26,152 @@ impl SugaredMorphismType {
         SugaredMorphismType {
             src_type: self.src_type.clone().normalize(),
             dst_type: self.dst_type.clone().normalize(),
+        }
+    }
+
+    pub fn strip_common_rungs(&self) -> SugaredMorphismType {
+        match (&self.src_type.clone().strip(), &self.dst_type.clone().strip()) {
+            (SugaredTypeTerm::Ladder(rungs_lhs), SugaredTypeTerm::Ladder(rungs_rhs)) => {
+
+                let mut lhs_iter = rungs_lhs.iter();
+                let mut rhs_iter = rungs_rhs.iter();
+                let mut last = SugaredMorphismType { src_type: SugaredTypeTerm::unit(), dst_type: SugaredTypeTerm::unit() };
+
+                while let (Some(lhs_top), Some(rhs_top)) = (lhs_iter.next(), rhs_iter.next()) {
+                    last = SugaredMorphismType{
+                        src_type: lhs_top.clone(),
+                        dst_type: rhs_top.clone()
+                    };
+
+                    if lhs_top != rhs_top {
+                        let x = SugaredMorphismType{ src_type: lhs_top.clone(), dst_type: rhs_top.clone() }.strip_common_rungs();
+
+                        let mut rl : Vec<_> = lhs_iter.cloned().collect();
+                        rl.insert(0, x.src_type);
+                        let mut rr : Vec<_> = rhs_iter.cloned().collect();
+                        rr.insert(0, x.dst_type);
+
+                        return SugaredMorphismType {
+                            src_type: SugaredTypeTerm::Ladder(rl),
+                            dst_type: SugaredTypeTerm::Ladder(rr)
+                        };
+                    }
+                }
+
+                last
+            }
+
+            (SugaredTypeTerm::Spec(args_lhs), SugaredTypeTerm::Spec(args_rhs)) => {
+
+                let (rl, rr) = args_lhs.iter().zip(args_rhs.iter()).map(
+                    |(al,ar)| SugaredMorphismType{ src_type: al.clone(), dst_type: ar.clone() }.strip_common_rungs()
+                )
+                .fold((vec![], vec![]), |(mut rl, mut rr), x| {
+                    rl.push(x.src_type);
+                    rr.push(x.dst_type);
+                    (rl,rr)
+                });
+
+                SugaredMorphismType {
+                    src_type: SugaredTypeTerm::Spec(rl),
+                    dst_type: SugaredTypeTerm::Spec(rr)
+                }
+            }
+
+            (SugaredTypeTerm::Seq { seq_repr:seq_repr_lhs, items:items_lhs },
+                SugaredTypeTerm::Seq { seq_repr: seq_repr_rhs, items:items_rhs })
+            => {
+                let (rl, rr) = items_lhs.iter().zip(items_rhs.iter()).map(
+                    |(al,ar)| SugaredMorphismType{ src_type: al.clone(), dst_type: ar.clone() }.strip_common_rungs()
+                )
+                .fold((vec![], vec![]), |(mut rl, mut rr), x| {
+                    rl.push(x.src_type);
+                    rr.push(x.dst_type);
+                    (rl,rr)
+                });
+                SugaredMorphismType  {
+                    src_type: SugaredTypeTerm::Seq{ seq_repr: seq_repr_lhs.clone(), items: rl },
+                    dst_type: SugaredTypeTerm::Seq { seq_repr: seq_repr_rhs.clone(), items: rr }
+                }
+            }
+
+            (SugaredTypeTerm::Struct { struct_repr:struct_repr_lhs, members:members_lhs },
+                SugaredTypeTerm::Struct { struct_repr: struct_repr_rhs, members:members_rhs })
+            => {
+                let mut rl = Vec::new();
+                let mut rr = Vec::new();
+
+                for ar in members_rhs.iter() {
+                    let mut found = false;
+                    for al in members_lhs.iter() {
+                        if al.symbol == ar.symbol {
+                            let x = SugaredMorphismType{ src_type: al.ty.clone(), dst_type: ar.ty.clone() }.strip_common_rungs();
+                            rl.push( SugaredStructMember{
+                                symbol: al.symbol.clone(),
+                                ty: x.src_type
+                            });
+                            rr.push( SugaredStructMember{
+                                symbol: ar.symbol.clone(),
+                                ty: x.dst_type
+                            });
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if !found {
+                        return SugaredMorphismType {
+                            src_type: SugaredTypeTerm::Struct { struct_repr: struct_repr_lhs.clone(), members:members_lhs.clone() },
+                            dst_type: SugaredTypeTerm::Struct { struct_repr: struct_repr_rhs.clone(), members:members_rhs.clone() }
+                        };
+                    }
+                }
+
+                SugaredMorphismType  {
+                    src_type: SugaredTypeTerm::Struct{ struct_repr: struct_repr_lhs.clone(), members: rl },
+                    dst_type: SugaredTypeTerm::Struct{ struct_repr: struct_repr_rhs.clone(), members: rr }
+                }
+            }
+
+            (SugaredTypeTerm::Enum { enum_repr:enum_repr_lhs, variants:variants_lhs },
+                SugaredTypeTerm::Enum { enum_repr: enum_repr_rhs, variants:variants_rhs })
+            => {
+                let mut rl = Vec::new();
+                let mut rr = Vec::new();
+
+                for ar in variants_rhs.iter() {
+                    let mut found = false;
+                    for al in variants_lhs.iter() {
+                        if al.symbol == ar.symbol {
+                            let x = SugaredMorphismType{ src_type: al.ty.clone(), dst_type: ar.ty.clone() }.strip_common_rungs();
+                            rl.push( SugaredEnumVariant{
+                                symbol: al.symbol.clone(),
+                                ty: x.src_type
+                            });
+                            rr.push( SugaredEnumVariant{
+                                symbol: ar.symbol.clone(),
+                                ty: x.dst_type
+                            });
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if !found {
+                        return SugaredMorphismType {
+                            src_type: SugaredTypeTerm::Enum { enum_repr: enum_repr_lhs.clone(), variants:variants_lhs.clone() },
+                            dst_type: SugaredTypeTerm::Enum { enum_repr: enum_repr_rhs.clone(), variants:variants_rhs.clone() }
+                        };
+                    }
+                }
+
+                SugaredMorphismType  {
+                    src_type: SugaredTypeTerm::Enum{ enum_repr: enum_repr_lhs.clone(), variants: rl },
+                    dst_type: SugaredTypeTerm::Enum { enum_repr: enum_repr_rhs.clone(), variants: rr }
+                }
+            }
+
+            (x,y) => SugaredMorphismType { src_type: x.clone(), dst_type: y.clone() }
         }
     }
 }
@@ -71,6 +213,10 @@ pub enum MorphismInstance2<M: SugaredMorphism + Clone> {
 
 
 impl<M: SugaredMorphism + Clone> MorphismInstance2<M> {
+    pub fn get_action_type(&self) -> SugaredMorphismType {
+        self.get_type().strip_common_rungs()
+    }
+
     pub fn get_type(&self) -> SugaredMorphismType {
         match self {
             MorphismInstance2::Primitive { ψ, σ, morph } => {
